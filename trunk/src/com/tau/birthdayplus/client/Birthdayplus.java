58a235102a58a225ef354e92a4678b5bf23990dd
princeeugene <princeeugene@55abf456-d003-11de-a306-d16691e8d855>
2009-11-20 03:45:04 +0000

    simple GWT interface with tabs and stack panel

--- trunk/src/com/tau/birthdayplus/client/Birthdayplus.java
+++ trunk/src/com/tau/birthdayplus/client/Birthdayplus.java
@@ -4,16 +4,20 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DecoratedStackPanel;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
@@ -38,74 +42,65 @@ public class Birthdayplus implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		final TabPanel  tab = new TabPanel();
+		tab.getElement().setId("tab");
+		
+		RootPanel.get("tabBarContainer").add(tab);
+		
+	    DecoratedStackPanel stackPanel = new DecoratedStackPanel();
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+	    // Add the birthdays
+	    String eugeneHeader = getHeaderString("Eugene");
+	    stackPanel.add(createFriendWishlist(), eugeneHeader, true);
+	    String olgaHeader = getHeaderString("Olga");
+	    stackPanel.add(createFriendWishlist(), olgaHeader, true);
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
+	    tab.add(stackPanel, "Events");
+		
+	    HTML wishlistText = new HTML("Wishlist");
+		tab.add(wishlistText, "Wishlist");
+		
+		tab.selectTab(0);
+		
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
+		final Button closeButton = new Button("Close");
+		closeButton.getElement().setId("closeButton");
 		dialogBox.setText("Remote Procedure Call");
 		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
 		final Label textToServerLabel = new Label();
 		final HTML serverResponseLabel = new HTML();
 		VerticalPanel dialogVPanel = new VerticalPanel();
 		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
+		dialogVPanel.add(new HTML("<b>Sending text to the server:</b>"));
 		dialogVPanel.add(textToServerLabel);
 		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
 		dialogVPanel.add(serverResponseLabel);
 		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
 		dialogVPanel.add(closeButton);
 		dialogBox.setWidget(dialogVPanel);
-
+		
 		// Add a handler to close the DialogBox
 		closeButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
 			}
 		});
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
+		// Create a handler for the tab
+		class MyHandler implements SelectionHandler {
 			/**
 			 * Fired when the user clicks on the sendButton.
 			 */
-			public void onClick(ClickEvent event) {
+			public void onSelection(SelectionEvent event) {
 				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
 			}
 
 			/**
 			 * Send the name from the nameField to the server and wait for a response.
 			 */
 			private void sendNameToServer() {
-				sendButton.setEnabled(false);
-				String textToServer = nameField.getText();
+				String textToServer = tab.getTabBar().getTitle();
 				textToServerLabel.setText(textToServer);
 				serverResponseLabel.setText("");
 				greetingService.greetServer(textToServer,
@@ -135,7 +130,54 @@ public class Birthdayplus implements EntryPoint {
 
 		// Add a handler to send the name to the server
 		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		tab.addSelectionHandler(handler);
 	}
+	
+	/**
+	   * Get a string representation of the header that includes an image and some
+	   * text.
+	   * 
+	   * @param text the header text
+	   * @param image the {@link AbstractImagePrototype} to add next to the header
+	   * @return the header as a string
+	   */
+	  private String getHeaderString(String text) {
+	    // Add the image and text to a horizontal panel
+	    HorizontalPanel hPanel = new HorizontalPanel();
+	    hPanel.setSpacing(0);
+	    hPanel.setVerticalAlignment(HasVerticalAlignment.ALIGN_MIDDLE);
+	    HTML headerText = new HTML(text);
+	    headerText.setStyleName("cw-StackPanelHeader");
+	    hPanel.add(headerText);
+
+	    // Return the HTML string for the panel
+	    return hPanel.getElement().getString();
+	  }
+	  
+	  /**
+	   * Create the wishlist of specific contact.
+	   * 
+	   * @return the wishlist
+	   */
+	  private VerticalPanel createFriendWishlist() {
+	    VerticalPanel itemsPanel = new VerticalPanel();
+	    itemsPanel.setSpacing(4);
+	    String[] items = {"Korova", "Krokodil", "Hryak"};
+	    for (int i = 0; i < items.length; i++) {
+	      final String item = items[i];
+	      final HTML contactLink = new HTML("<a href=\"javascript:undefined;\">"
+	          + item + "</a>");
+	      itemsPanel.add(contactLink);
+
+	      // Open the contact info popup when the user clicks a contact
+	      contactLink.addClickHandler(new ClickHandler() {
+	        public void onClick(ClickEvent event) {
+		         Window.alert("Clicked");
+	        }
+	      });
+	    }
+	    return itemsPanel;
+	  }
+
+
 }
