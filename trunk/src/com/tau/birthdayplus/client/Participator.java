2cadb286673b5e1d54559b012033c96db585d903
johanzilber <johanzilber@55abf456-d003-11de-a306-d16691e8d855>
2009-11-23 08:45:41 +0000

    data base classes added

---
+++ trunk/src/com/tau/birthdayplus/client/Participator.java
@@ -0,0 +1,57 @@
+package com.tau.birthdayplus.client;
+
+import java.util.List;
+
+import com.google.appengine.api.users.User;
+
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+public class Participator{
+	   @PrimaryKey
+	   @Persistent
+	private
+	   String userId;
+	   @Persistent
+	private
+	   Integer money;
+	   @Persistent
+	   List<Vote> votes;
+	   
+	   
+	   public Participator(String id, Integer money){
+		   this.userId = id;
+		   this.setMoney(money);
+	   }
+	   
+	   public Participator(User googleAccount, Integer money) {
+		   this(googleAccount.getUserId(), money);
+	   }
+
+	   public Participator(Guest guest, Integer money) {
+		   this(guest.getId(), money);
+	   }
+	   
+	   public List<Vote> getVotes(){
+		   return votes;
+	   }
+
+	/*void setUserId(String userId) {
+		this.userId = userId;
+	}*/
+
+	String getUserId() {
+		return userId;
+	}
+
+	void setMoney(Integer money) {
+		this.money = money;
+	}
+
+	Integer getMoney() {
+		return money;
+	}
+}
