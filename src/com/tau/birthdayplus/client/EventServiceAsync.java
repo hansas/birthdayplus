ab9347abc304678a7ae69363a1bd816688364c62
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-11-23 21:02:27 +0000

    update to event interface

--- src/com/tau/birthdayplus/client/EventServiceAsync.java
+++ src/com/tau/birthdayplus/client/EventServiceAsync.java
@@ -1,6 +1,9 @@
 package com.tau.birthdayplus.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.tau.birthdayplus.domain.Event;
 
 /**
  * The async counterpart of <code>EventService</code>.
@@ -11,5 +14,24 @@ public interface EventServiceAsync {
 	 * function that checks that client can call to the remote service
 	 */
 	void printHello(AsyncCallback<Void> callback);
+	/*
+	 * creates new event for the given user
+	 */
+	void createEvent(String uId, Event event, AsyncCallback<Void> callback);
+	/*
+	 * update the event
+	 * check if the user is the event owner
+	 */
+	void updateEvent(String uId, Event event, AsyncCallback<Void> callback);
+	/*
+	 * delete the event 
+	 * check if the user is the event owner
+	 */
+	void deleteEvent(String uId, Event event, AsyncCallback<Void> callback);
+	/*
+	 * get events by user id
+	 */
+	void getEvents(String uId, AsyncCallback<ArrayList<Event>> callback);
+
 
 }
