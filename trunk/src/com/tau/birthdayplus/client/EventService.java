7babe947c65162d860a9f8e3e183b3c75c19e98b
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-11-23 21:02:27 +0000

    update to event interface

--- trunk/src/com/tau/birthdayplus/client/EventService.java
+++ trunk/src/com/tau/birthdayplus/client/EventService.java
@@ -1,7 +1,11 @@
 package com.tau.birthdayplus.client;
 
+import java.util.ArrayList;
+
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+import com.tau.birthdayplus.domain.Event;
 
 /**
  * The client side stub for the RPC service.
@@ -12,5 +16,23 @@ public interface EventService extends RemoteService{
 	 * function that checks that client can call to the remote service
 	 */
 	void printHello();
+	/*
+	 * creates new event for the given user
+	 */
+	void createEvent(String uId,Event event);
+	/*
+	 * update the event
+	 * check if the user is the event owner
+	 */
+	void updateEvent(String uId,Event event);
+	/*
+	 * delete the event 
+	 * check if the user is the event owner
+	 */
+	void deleteEvent(String uId,Event event);
+	/*
+	 * get events by user id
+	 */
+	ArrayList<Event> getEvents(String uId);
 
 }
