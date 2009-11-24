acd209e26e34a8400514862acb2ef2a1cffdc3e2
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-11-24 13:38:07 +0000

    services implementations

--- src/com/tau/birthdayplus/server/GreetingServiceImpl.java
+++ src/com/tau/birthdayplus/server/GreetingServiceImpl.java
@@ -16,71 +16,12 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class GreetingServiceImpl extends RemoteServiceServlet implements
 		GreetingService {
 
-	public String greetServer(String input) {
-		String serverInfo = getServletContext().getServerInfo();
-		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
-	}
-/*
-	@Override
-	public Guest createGuest(Guest guest) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public Guest updateGuest(Guest guest) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-    */
+	/*
 	
-	public Event createEvent(Event event, String uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
- 
-	public WishlistItem createWishlistItem(WishlistItem wItem, String uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-    /*
-	@Override
-	public Event deleteEvent(Event event, String uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public WishlistItem deleteWishlistItem(WishlistItem wItem, String uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<Event> getEventList(String uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-    */
 	public Guest loadGuest(String guestId) {
 		return UserManagement.loadGuest(guestId);
 	}
-    /*
-	@Override
-	public Event updateEvent(Event event, String uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public WishlistItem updateWishlistItem(WishlistItem wItem, String uid) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	*/
+    */
+	
 }
