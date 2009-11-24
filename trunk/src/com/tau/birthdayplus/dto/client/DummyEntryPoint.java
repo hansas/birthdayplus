c27f013f91ce2520dd4b8c49fd18db965911e0df
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-11-24 13:35:55 +0000

    new folder for model

---
+++ trunk/src/com/tau/birthdayplus/dto/client/DummyEntryPoint.java
@@ -0,0 +1,12 @@
+package com.tau.birthdayplus.dto.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class DummyEntryPoint implements EntryPoint {
+
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
