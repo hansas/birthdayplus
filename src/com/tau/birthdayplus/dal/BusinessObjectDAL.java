757bb8f1517aefe54c269f21980027918f3985ae
irenalet@gmail.com <irenalet@gmail.com@55abf456-d003-11de-a306-d16691e8d855>
2009-11-25 12:26:04 +0000

    updateEvent() was implemented

--- src/com/tau/birthdayplus/dal/BusinessObjectDAL.java
+++ src/com/tau/birthdayplus/dal/BusinessObjectDAL.java
@@ -1,7 +1,10 @@
 package com.tau.birthdayplus.dal;
 
 import javax.jdo.PersistenceManager;
+import javax.jdo.Transaction;
 
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.tau.birthdayplus.domain.Event;
 import com.tau.birthdayplus.domain.Guest;
 import com.tau.birthdayplus.dto.client.EventData;
@@ -22,10 +25,34 @@ public class BusinessObjectDAL {
 		return g;
 	}
 
-	public static void updateEvent(EventData event) {
-		// TODO Auto-generated method stub
-		
+	public static void updateEvent(EventData eventD) {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Transaction tx = (Transaction)pm.currentTransaction();
+		try
+		{
+		    tx.begin();
+		    Key eventKey = KeyFactory.stringToKey(eventD.getEventId());
+   		    Event e = new Event(eventD);
+		    e.setKey(eventKey);
+		    pm.makePersistent(e);
+		    tx.commit();
+		}
+		finally
+		{
+		    if (tx.isActive())
+		    {
+		        tx.rollback();
+		    }
+		}
+		pm.close();
+		throw new RuntimeException(new Exception("You cannot update someone else's event "));
 	}
+	
+	//checks if user with id uId has eventD in his events
+//	public static Boolean isMyEvent (String uId, EventData eventD){
+//		Key parentKey = KeyFactory.stringToKey(eventD.getEventId());
+//		return uId.equals(parentKey);
+//	}
 	
 	public static void createEvent(EventData eventD){
 		Event event = new Event(eventD);
