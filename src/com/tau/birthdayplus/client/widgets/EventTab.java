a5fed9d4efd742c122518f9abc1dde05263af76d
princeeugene <princeeugene@55abf456-d003-11de-a306-d16691e8d855>
2009-11-25 18:35:44 +0000

    added package for gui widgets

---
+++ src/com/tau/birthdayplus/client/widgets/EventTab.java
@@ -0,0 +1,12 @@
+package com.tau.birthdayplus.client.widgets;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class EventTab extends Widget {
+	VerticalPanel vPanel;
+	
+	public EventTab(){
+		
+	}
+}
