bf660b4a1a39d663c8102460c6223e13b594fd09
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-11-27 14:05:19 +0000

    with 2 tabs, wishlist and i buy

--- src/com/tau/birthdayplus/client/Birthdayplus.java
+++ src/com/tau/birthdayplus/client/Birthdayplus.java
@@ -1,69 +1,43 @@
 package com.tau.birthdayplus.client;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+
+
+
+import java.util.HashMap;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.i18n.client.LocaleInfo;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DecoratedStackPanel;
-import com.google.gwt.user.client.ui.DecoratorPanel;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.FlexTable;
+
+
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+
 import com.google.gwt.user.client.ui.HTMLTable.Cell;
-import com.google.gwt.user.datepicker.client.DateBox;
+
 import com.tau.birthdayplus.client.widgets.EventTabDelegate;
 import com.tau.birthdayplus.client.widgets.EventTabGUI;
 import com.tau.birthdayplus.client.widgets.MyWishlistDelegate;
 import com.tau.birthdayplus.client.widgets.MyWishlistTabGUI;
-import com.tau.birthdayplus.dto.client.EventData;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Birthdayplus implements EntryPoint {
 	CwConstants constants = GWT.create(CwConstants.class);
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
+	
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
+	/*
+	 * user id and HashMap of <friend id, friend name> from open social
 	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
-	/**
-	 * Create a remote service proxy to talk to the server-side Event service.
-	 */
-	private final EventServiceAsync eventService = GWT.create(EventService.class); 
-	
-	/**
-	 * Create a remote service proxy to talk to the server-side Profile service.
-	 */
-	private final ProfileServiceAsync profileService = GWT.create(ProfileService.class); 
-	
-	/**
-	 * Create a remote service proxy to talk to the server-side Profile service.
-	 */
-	private final WishlistServiceAsync wishlistService = GWT.create(WishlistService.class); 
+	public String userId="";
+	public HashMap<String,String> userFriends= new HashMap<String,String>();
 	
 	/*
 	 *  Event tab
@@ -72,136 +46,83 @@ public class Birthdayplus implements EntryPoint {
 	private EventTabDelegate eventDeligate;
 	
 	/*
-	 * for second tab
+	 * MyWishlistTab tab
 	 */
-	private MyWishlistTabGUI gui;
-	private MyWishlistDelegate delegate;
+	private MyWishlistTabGUI myWishlistGUI;
+	private MyWishlistDelegate myWishlistDelegate;
 	
 	/*
-	 * listen to the widgets of second tab
+	 * IBuy Tab
 	 */
-	private void wireGUIEvents() {
-        gui.wishTable.addClickHandler(new ClickHandler(){
-            public void onClick(ClickEvent event) {
-                 Cell cellForEvent = gui.wishTable.getCellForEvent(event);
-                 gui.gui_eventItemGridClicked(cellForEvent);                
-            }});
-        
-        gui.addItemButton.addClickHandler(new ClickHandler(){
-            public void onClick(ClickEvent event) {
-            	gui.gui_eventAddItemButtonClicked();
-            }});
-
-        gui.updateButton.addClickHandler(new ClickHandler(){
-            public void onClick(ClickEvent event) {
-                gui.gui_eventUpdateButtonClicked();
-            }});
-        
-        gui.addButton.addClickHandler(new ClickHandler(){
-            public void onClick(ClickEvent event) {
-            	gui.gui_eventAddButtonClicked();
-                
-            }});
-        
-        gui.cancelButton.addClickHandler(new ClickHandler(){
-        	public void onClick(ClickEvent event){
-        		gui.gui_eventCancelButtonClicked();
-        	}
-        });
-        
-       /* eventGui.btnAddEvent.addClickHandler(new ClickHandler(){
-        	public void onClick(ClickEvent event){
-        		eventGui.gui_eventAddEventButtonClicked();
-        	}
-        });
-        
-*/
-    }
-
-
+	private com.tau.birthdayplus.client.widgets.IBuyTabGUI iBuyGUI;
+	private com.tau.birthdayplus.client.widgets.IBuyDelegate iBuyDelegate;
+	
+	
+	
+	
+	
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final TabPanel  tab = new TabPanel();
-		List<Person> listFriends = new ArrayList<Person>();
-
-		tab.getElement().setId("tab");
-		tab.setAnimationEnabled(true);
+		TabPanel tab = new TabPanel();
+		//	List<Person> listFriends = new ArrayList<Person>();
+			tab.getElement().setId("tab");
+			tab.setAnimationEnabled(true);
+		//	RootPanel.get("tabBarContainer").add(tab);
+			RootPanel.get().add(tab);
+			
+			
+			
+		//    listFriends.add(new Person("1", "Eugene"));
+		//    listFriends.add(new Person("2", "Olga"));
+		//    listFriends.add(new Person("3", "Irena"));
+		//    listFriends.add(new Person("4", "Eugene 2"));
+		    
+		    /**
+		     *  Add events tab
+		     */
+		// /   List<EventData> list = new ArrayList<EventData>(); 
+		//    EventData event = new EventData("1","smert", new Date(2000), false);
+		 //   list.add(event);
+		    
+		    //eventGui = new EventTabGUI(list); 
+		    //eventDeligate = new EventTabDeligate();
+		    //eventGui.eventService = eventDeligate;
+		    //tab.add(eventGui, "Events");
+			
+			
+		    //add MyWishlist to the second tab
+		    myWishlistGUI = new MyWishlistTabGUI();
+		    myWishlistDelegate = new MyWishlistDelegate();
+	        myWishlistGUI.wishlistService = myWishlistDelegate;
+	        myWishlistDelegate.gui =myWishlistGUI;
+	        myWishlistGUI.init();
+	        
+			tab.add(myWishlistGUI.wishlistVerticalPanel, "Wishlist");
+			
+			
+			//add IBuy to the second tab
+		    iBuyGUI = new com.tau.birthdayplus.client.widgets.IBuyTabGUI();
+		    iBuyDelegate = new com.tau.birthdayplus.client.widgets.IBuyDelegate();
+		    iBuyGUI.wishlistService =iBuyDelegate;
+		    iBuyDelegate.gui =iBuyGUI;
+		    iBuyGUI.init();
+	        
+			tab.add(iBuyGUI.wishlistVerticalPanel, "I've booked this items");
+			
 		
-		RootPanel.get("tabBarContainer").add(tab);
+			//listen to the events in the tabs
+			wireMyWishlistGUIEvents();
+			wireIBuyGUIEvents();
+			
+			tab.selectTab(0);
 		
 		
-	    listFriends.add(new Person("1", "Eugene"));
-	    listFriends.add(new Person("2", "Olga"));
-	    listFriends.add(new Person("3", "Irena"));
-	    listFriends.add(new Person("4", "Eugene 2"));
-	    
-	    /**
-	     *  Add events tab
-	     */
-	    List<EventData> list = new ArrayList<EventData>(); 
-	    EventData event = new EventData("1","smert", new Date(2000), false);
-	    list.add(event);
-	    
-	    //eventGui = new EventTabGUI(list); 
-	    //eventDeligate = new EventTabDeligate();
-	    //eventGui.eventService = eventDeligate;
-	    //tab.add(eventGui, "Events");
 		
-	    /*
-	     * add MyWishlist to the second tab
-	     */
-	    gui = new MyWishlistTabGUI();
-        delegate = new MyWishlistDelegate();
-        gui.wishlistService = delegate;
-        delegate.gui = gui;
-        gui.init();
-        
-		tab.add(gui.wishlistVerticalPanel, "Wishlist");
 		
-		//listen to the events in second tab
-		wireGUIEvents();
 		
-		HTML buyListText = new HTML("Need to buy");
-		tab.add(buyListText, "Need to buy");
-		
-		tab.selectTab(0);
-
-		//check that can communicate with remote services 
-		eventService.printHello(
-				new AsyncCallback<Void>() {
-					public void onFailure(Throwable caught) {
-						
-					}
-
-					public void onSuccess(Void result) {
-						// TODO Auto-generated method stub
-						
-					}
-				});
-		
-		profileService.printHello(new AsyncCallback<Void>() {
-			public void onFailure(Throwable caught) {
-				
-			}
-
-			public void onSuccess(Void result) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		wishlistService.printHello(new AsyncCallback<Void>() {
-			public void onFailure(Throwable caught) {
-				
-			}
-
-			public void onSuccess(Void result) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
 	}
 	
 	/**
@@ -225,5 +146,52 @@ public class Birthdayplus implements EntryPoint {
 	    return hPanel.getElement().getString();
 	  }
 	  
+	  
+
+		/*
+		 * listen to the widgets of MyWishlist tab
+		 */
+		private void wireMyWishlistGUIEvents() {
+			myWishlistGUI.wishTable.addClickHandler(new ClickHandler(){
+	            public void onClick(ClickEvent event) {
+	                 Cell cellForEvent = myWishlistGUI.wishTable.getCellForEvent(event);
+	                 myWishlistGUI.gui_eventItemGridClicked(cellForEvent);                
+	            }});
+	        
+			myWishlistGUI.addItemButton.addClickHandler(new ClickHandler(){
+	            public void onClick(ClickEvent event) {
+	            	myWishlistGUI.gui_eventAddItemButtonClicked();
+	            }});
+
+			myWishlistGUI.updateButton.addClickHandler(new ClickHandler(){
+	            public void onClick(ClickEvent event) {
+	            	myWishlistGUI.gui_eventUpdateButtonClicked();
+	            }});
+	        
+			myWishlistGUI.addButton.addClickHandler(new ClickHandler(){
+	            public void onClick(ClickEvent event) {
+	            	myWishlistGUI.gui_eventAddButtonClicked();
+	                
+	            }});
+	        
+			myWishlistGUI.cancelButton.addClickHandler(new ClickHandler(){
+	        	public void onClick(ClickEvent event){
+	        		myWishlistGUI.gui_eventCancelButtonClicked();
+	        	}
+	        });
+	  
+		}
+		
+		/*
+		 * listen to the widgets of I've booked this items tab
+		 */
+		private void wireIBuyGUIEvents(){
+			iBuyGUI.wishTable.addClickHandler(new ClickHandler(){
+				public void onClick(ClickEvent event){
+					Cell cellForEvent=iBuyGUI.wishTable.getCellForEvent(event);
+					iBuyGUI.gui_eventItemGridClicked(cellForEvent);
+				}
+			});
+		}
 	  
 }
