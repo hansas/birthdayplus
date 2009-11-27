8e52597db258d0f818832710c13bcb02b145d402
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-11-27 06:32:20 +0000

    name has been changed : from deligate to delegate

---
+++ trunk/src/com/tau/birthdayplus/client/widgets/MyWishlistDelegate.java
@@ -0,0 +1,68 @@
+package com.tau.birthdayplus.client.widgets;
+
+import java.util.ArrayList;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.tau.birthdayplus.client.WishlistService;
+import com.tau.birthdayplus.client.WishlistServiceAsync;
+import com.tau.birthdayplus.dto.client.WishlistItemData;
+
+public class MyWishlistDelegate {
+	private final WishlistServiceAsync wishlistService = GWT.create(WishlistService.class); 
+	public MyWishlistTabGUI gui;
+	 void getWishlist(final String uIdlist) {
+	    	wishlistService.getWishlist(uIdlist, new AsyncCallback<ArrayList<WishlistItemData>>(){
+
+	    		public void onFailure(Throwable caught){
+	    			gui.service_eventGetWishlistFailed(caught);
+	    		}
+	    		public void onSuccess(ArrayList<WishlistItemData> result){
+	    			gui.service_eventGetWishlistSuccesfull(result);
+	    		}
+	    	}//end of inner class
+	    	);//end of method call
+	    	}
+	       
+	    
+	    
+	    void createWishlistItem(final WishlistItemData item){
+	    	wishlistService.createWishlistItem(item, new AsyncCallback<Void>(){
+	    		public void onFailure(Throwable caught){
+	    			gui.service_eventCreateWishlistItemFailed(caught);
+	    		}
+	    		
+	    		public void onSuccess(Void result){
+	    			gui.service_eventCreateWishlistItemSuccessful();
+	    		}
+	    	}//end of inner class
+	    );//end of method call
+	    }
+	    
+	    void updateWishlistItem(final WishlistItemData item){
+	    	wishlistService.updateWishlistItem(item, new AsyncCallback<Void>(){
+	    		public void onFailure(Throwable caught){
+	    			gui.service_eventUpdateWishlistItemFailed(caught);
+	    		}
+	    		
+	    		public void onSuccess(Void result){
+	    			gui.service_eventUpdateWishlistItemSuccessful();
+	    		}
+	    	}//end of inner class
+	    	);//end of method call
+	    }
+
+	   void deleteWishlistItem(final String wishlistItemId){
+		   wishlistService.deleteWishlistItem(wishlistItemId, new AsyncCallback<Void>(){
+			   public void onFailure(Throwable caught){
+				   gui.service_deleteWishlistItemFailed(caught);
+			   }
+			   
+			   public void onSuccess(Void result){
+				   gui.service_deleteWishlistItemSuccessful();
+			   }
+		   }//end of inner class
+		   );//end of method call
+	   }    
+
+}
