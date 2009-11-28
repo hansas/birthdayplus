297c797d7841e14233053940ede199356c4442bd
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-11-28 10:40:11 +0000

    all the tabs are ready, connecting to server for events.

--- trunk/src/com/tau/birthdayplus/client/Birthdayplus.java
+++ trunk/src/com/tau/birthdayplus/client/Birthdayplus.java
@@ -3,12 +3,15 @@ package com.tau.birthdayplus.client;
 
 
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 
 
@@ -24,6 +27,8 @@ import com.tau.birthdayplus.client.widgets.EventTabDelegate;
 import com.tau.birthdayplus.client.widgets.EventTabGUI;
 import com.tau.birthdayplus.client.widgets.MyWishlistDelegate;
 import com.tau.birthdayplus.client.widgets.MyWishlistTabGUI;
+import com.tau.birthdayplus.dto.client.EventData;
+import com.tau.birthdayplus.dto.client.GuestData;
 
 
 /**
@@ -32,11 +37,13 @@ import com.tau.birthdayplus.client.widgets.MyWishlistTabGUI;
 public class Birthdayplus implements EntryPoint {
 	CwConstants constants = GWT.create(CwConstants.class);
 	
-
+	// Create a remote service proxy to talk to the server-side Profile service.
+	private final ProfileServiceAsync profileService = GWT.create(ProfileService.class); 
+	private final EventServiceAsync eventService = GWT.create(EventService.class); 
 	/*
 	 * user id and HashMap of <friend id, friend name> from open social
 	 */
-	public String userId="";
+	public String userId="555";
 	public HashMap<String,String> userFriends= new HashMap<String,String>();
 	
 	/*
@@ -66,59 +73,63 @@ public class Birthdayplus implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		TabPanel tab = new TabPanel();
-		//	List<Person> listFriends = new ArrayList<Person>();
+		    TabPanel tab = new TabPanel();
+		
 			tab.getElement().setId("tab");
 			tab.setAnimationEnabled(true);
 		//	RootPanel.get("tabBarContainer").add(tab);
 			RootPanel.get().add(tab);
 			
-			
-			
-		//    listFriends.add(new Person("1", "Eugene"));
-		//    listFriends.add(new Person("2", "Olga"));
-		//    listFriends.add(new Person("3", "Irena"));
-		//    listFriends.add(new Person("4", "Eugene 2"));
-		    
-		    /**
-		     *  Add events tab
-		     */
-		// /   List<EventData> list = new ArrayList<EventData>(); 
-		//    EventData event = new EventData("1","smert", new Date(2000), false);
-		 //   list.add(event);
-		    
+			//initiate event tab
 		    eventGui = new EventTabGUI(); 
 		    eventDelegate = new EventTabDelegate();
 		    eventGui.eventService = eventDelegate;
 		    eventDelegate.eventGui = eventGui;
+		    eventGui.entryPoint=this;
 		    eventGui.init();
-		    tab.add(eventGui, "Events");
+		    tab.add(eventGui.vPanel, "Events");
 			
 			
-		    //add MyWishlist to the second tab
+		    //initiate My wIwishlist tab
 		    myWishlistGUI = new MyWishlistTabGUI();
 		    myWishlistDelegate = new MyWishlistDelegate();
 	        myWishlistGUI.wishlistService = myWishlistDelegate;
 	        myWishlistDelegate.gui =myWishlistGUI;
+	        myWishlistGUI.entryPoint=this;
 	        myWishlistGUI.init();
-	        
 			tab.add(myWishlistGUI.wishlistVerticalPanel, "Wishlist");
 			
 			
-			//add IBuy to the second tab
+			//initiate I Buy tab
 		    iBuyGUI = new com.tau.birthdayplus.client.widgets.IBuyTabGUI();
 		    iBuyDelegate = new com.tau.birthdayplus.client.widgets.IBuyDelegate();
 		    iBuyGUI.wishlistService =iBuyDelegate;
 		    iBuyDelegate.gui =iBuyGUI;
+		    iBuyGUI.entryPoint=this;
 		    iBuyGUI.init();
 	        
-			tab.add(iBuyGUI.wishlistVerticalPanel, "I've booked this items");
+			tab.add(iBuyGUI.wishlistVerticalPanel, "I buy");
 			
-		
-			//listen to the events in the tabs
-			wireEventGUIEvents();
-			wireMyWishlistGUIEvents();
-			wireIBuyGUIEvents();
+			
+			//GuestData user= new GuestData(userId,"olga","vingurt",new Date());
+		//	profileService.createProfile(user, new AsyncCallback<Void>(){
+	    	//	public void onFailure(Throwable caught){
+	    //			System.out.println(caught);
+	    //		}
+	    //		
+		//		public void onSuccess(Void result) {
+					ArrayList<String> temp=new ArrayList<String>();
+					temp.add(userId);
+					eventDelegate.getEvents(temp);
+					//listen to the events in the tabs
+	    			wireEventGUIEvents();
+	    			wireMyWishlistGUIEvents();
+	    			wireIBuyGUIEvents();
+					
+			//	}
+				
+	  //  	}//end of inner class
+	   // 	);//end of method call)
 			
 			tab.selectTab(0);
 		
@@ -201,9 +212,30 @@ public class Birthdayplus implements EntryPoint {
 			eventGui.eventTable.addClickHandler(new ClickHandler(){
 				public void onClick(ClickEvent event){
 					Cell cellForEvent = eventGui.eventTable.getCellForEvent(event);
-					eventGui.gui_eventTableEventClicked(cellForEvent);
+					eventGui.gui_eventEventGridClicked(cellForEvent);
 				}
 			});
+			eventGui.btnAddEvent.addClickHandler(new ClickHandler(){
+	            public void onClick(ClickEvent event) {
+	            	eventGui.gui_eventAddEventButtonClicked();
+	            }});
+
+			eventGui.updateButton.addClickHandler(new ClickHandler(){
+	            public void onClick(ClickEvent event) {
+	            	eventGui.gui_eventUpdateButtonClicked();
+	            }});
+	        
+			eventGui.addButton.addClickHandler(new ClickHandler(){
+	            public void onClick(ClickEvent event) {
+	            	eventGui.gui_eventAddButtonClicked();
+	                
+	            }});
+	        
+			eventGui.cancelButton.addClickHandler(new ClickHandler(){
+	        	public void onClick(ClickEvent event){
+	        		eventGui.gui_eventCancelButtonClicked();
+	        	}
+	        });
 		}
 	  
 }
