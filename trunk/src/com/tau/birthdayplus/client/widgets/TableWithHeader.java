c4b17a0ca13166f66ad4500dff3c1488f4273677
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-12-11 19:32:44 +0000

    gui changes

--- trunk/src/com/tau/birthdayplus/client/widgets/TableWithHeader.java
+++ trunk/src/com/tau/birthdayplus/client/widgets/TableWithHeader.java
@@ -17,7 +17,7 @@ public class TableWithHeader  extends FlexTable{
 	  DOM.insertChild(head, headerTr, 0);  
 	  Element tBody = getBodyElement();  
 	  DOM.setElementAttribute(tBody, "style", "overflow:auto;text-align: center;");  
-	  DOM.setElementAttribute(head, "style", "text-align: center;");  
+	  DOM.setElementAttribute(head, "style", "text-align: center;height:14px; font-size:13px;color:#473C8B;font-family:Arial;font-weight:bold;");
 	  
 	 }  
 	   
