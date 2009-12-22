66feee7e8d269aa6c7dc1c7c0345a92674420c3b
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-12-22 12:44:01 +0000

    grease monkey

--- trunk/src/com/tau/birthdayplus/server/AddWishlistItem.java
+++ trunk/src/com/tau/birthdayplus/server/AddWishlistItem.java
@@ -1,7 +1,12 @@
 package com.tau.birthdayplus.server;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+
+import com.tau.birthdayplus.dto.client.WishlistItemData;
 
 
 public class AddWishlistItem  extends HttpServlet {
@@ -33,13 +40,15 @@ public class AddWishlistItem  extends HttpServlet {
         log.info(req.getParameter("href"));
         
         if (user != null) {
-        log.info("I know you");
-        log.info("query string"+req.getQueryString());
-        log.info(req.getParameter("href"));
-        
-      //  resp.setContentType("text/plain");
+        try{
+        	WishlistItemData  data = new WishlistItemData();
+        	Parse(data, req.getParameter("href"));
+        	
+        }catch(Exception ex){
+        	
+        }
         resp.setContentType("text/html; charset=UTF-8");
-        String html = "<div id='pnl_AfterAction'><table width='100%'><tr><td  align='center' class='ablack' dir='rtl'><b><br />	hamozar nimhak beazlaha<br /><br /><a href='javascript:void(0);' onClick='window.close()'><u>lahaz kan lesgiral hahalon</u></a></b></td></tr></table></div></div>";
+        String html = "<div id='pnl_AfterAction'><table width='100%'><tr><td  align='center' class='ablack' dir='rtl'><b><br />המוצר שנבחר התווסף לרשימה שלך בהצלחה. <br /><br /><a href='javascript:void(0);' onClick='window.close()'><u>לחץ כאן לסגירת החלון</u></a></b></td></tr></table></div></div>";
         resp.getWriter().println(html);
      //   resp.getWriter().println("Hello, " + user.getNickname()+"your want to add "+req.getParameter("href"));
         } else {
@@ -51,6 +60,42 @@ public class AddWishlistItem  extends HttpServlet {
         	resp.sendRedirect(userService.createLoginURL(req.getRequestURI()+"?href="+req.getParameter("href")));
          }
        }
+	
+	
+	private static void Parse(WishlistItemData item,String url){
+		URLConnection connection = null;
+		try {
+		  connection =  new URL(url).openConnection();
+		  Scanner scanner = new Scanner(connection.getInputStream());
+		  while ( scanner.hasNextLine() ){
+		        if(findLine( scanner.nextLine(),item))
+		        		break;
+		      }
+	
+		}catch ( Exception ex ) {
+		    ex.printStackTrace();
+		}
+		
+
+	}
+	
+	
+	 private static boolean findLine(String aLine,WishlistItemData item){
+		    Pattern pattern = Pattern.compile("onSessionCreated, connected, loginUser,'(\\d)+','[\\w ]+','[\\w/.]+'" );
+		    Matcher matcher = pattern.matcher( aLine );
+
+		    if (matcher.find() ){
+		      String[] temp = matcher.group(0).split("[,;]");
+		      item.setItemName(temp[4].replace("'", " ").trim());
+		      log.info("item name is : "+item.getItemName());
+		      item.setThumbnail("http://img.zap.co.il/pics/"+temp[5].replace("'", " ").trim());
+		      log.info("item thumbnail is : "+item.getThumbnail());
+		      return true;
+		    }
+		    return false;
+		    
+		  }
+
 
 
 }
