6875caadaf084e7067459e65e75a46d460d53752
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2009-12-27 16:14:28 +0000

    widgets for gadget

---
+++ src/com/tau/birthdayplus/client/widgets/FlowPanelMenuTitle.java
@@ -0,0 +1,58 @@
+package com.tau.birthdayplus.client.widgets;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
+
+public class FlowPanelMenuTitle extends FlowPanel{
+	private MenuBar menu ;
+    private Label title;
+    
+    
+    FlowPanelMenuTitle(){
+    	setStyleName("Panel");
+    	title = new Label("");
+		add(title);
+		title.addStyleName("Label");
+		
+		menu = new MenuBar();
+		add(menu);
+		menu.addStyleName("buttonPanel");
+		menu.setAutoOpen(true);
+		menu.setAnimationEnabled(true);
+    	
+    }
+    
+    public void setTitle(String title){
+    	this.title.setText(title);
+    }
+    
+    public MenuItem addMenuItem(MenuItem item){
+    	return menu.addItem(item);
+    }
+    
+    public MenuItem addMenuItem(String text,boolean asHTML,Command cmd){
+    	return this.menu.addItem(text, asHTML, cmd);
+    	
+    }
+    
+    public MenuItem addMenuItem(String text,boolean asHTML,MenuBar popup){
+    	return this.menu.addItem(text, asHTML, popup);
+    	
+    }
+    
+    public MenuItem addMenuItem(String text,Command cmd){
+    	return this.menu.addItem(text, cmd);
+    }
+    
+    public MenuItem addMenuItem(String text,MenuBar popup){
+    	return this.menu.addItem(text, popup);
+    }
+    
+    
+    
+    
+
+}
