ec5b795c144a02b4e0ed034f4f024982d6a68d24
olga.vingurt <olga.vingurt@55abf456-d003-11de-a306-d16691e8d855>
2010-01-15 17:52:56 +0000

    update for crud from google calendar

--- trunk/src/com/tau/birthdayplus/server/AddEventServlet.java
+++ trunk/src/com/tau/birthdayplus/server/AddEventServlet.java
@@ -35,72 +35,12 @@ public class AddEventServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final Logger logger = Logger.getLogger(AddEventServlet.class.getName());
 	
-	 /**
-     * The default buffer size to use.
-     */
-	/*
-	 * Invitation: vcxv dgdg @ Annually on June 6 (event@testrpcplus.appspotmail.com)
-	 * Invitation: kkk kk skk @ Annually from 9pm to 10pm on June 26 (event@testrpcplus.appspotmail.com)
-	 */
-	private static final Pattern eventAnnualyPattern  = Pattern.compile("(?:Invitation:) ([\\p{L} ]+) (?:@ Annually(?: from [\\w:]+ to [\\w:]+)? on) (\\w{3,}) (\\d{1,2}) (\\(event@birthdayplus.appspotmail.com\\))");
-	/*
-	 * Invitation: something @ Thu Dec 24, 2009 (event@testrpcplus.appspotmail.com)
-	 * Invitation: something @ Sat Jan 23 8:30am - 9:30am (event@testrpcplus.appspotmail.com)
-	 */
-    private static final Pattern eventOncePattern = Pattern.compile("(?:Invitation:) ([\\p{L} ]+) (?:@ \\w+) (\\w{3,}) (\\d{1,2})(?:, \\d+)?(?: [\\w :-]+)? (\\(event@birthdayplus.appspotmail.com\\))");
-
-	/*
-	 * Olya Vingurt <yalo_niv@yahoo.com>
-	 */
-    private static final Pattern emailPattern = Pattern.compile("(?:[\\w ]+) (?:<)([\\w@.]+)(>)");
-
-
-	
-    private enum Month {
-		
-		Jan(0,"Jan","January"),
-		Feb(1,"Feb","February"),
-		Mar(2,"Mar","March"),
-		Apr(3,"Apr","April"),
-		May(4,"May","May"),
-		June(5,"June","June"),
-		July(6,"July","July"),
-		Aug(7,"Aug","August"),
-		Sep(8,"Sep","September"),
-		Oct(9,"Oct","October"),
-		Nov(10,"Nov","November"),
-		Dec(11,"Dec","December");
-		
-		
-		private int index;
-		private String shortName;
-		private String fullName;
-		private Month(int index,String shortName,String fullName){
-			this.index = index;
-			this.shortName = shortName;
-			this.fullName = fullName;
-		}
-		
-		
-		public static int getIndex(String name){
-			for(Month month : Month.values() ){
-				if(month.shortName.equals(name) || month.fullName.equals(name))
-					return month.index;
-			}
-			return -1;
-			
-		}
-	}
-	
-	/**
-	 * 
-	 */
-	
 	
 	
     public void doPost(HttpServletRequest req, HttpServletResponse resp)  throws IOException {
     	 Properties props = new Properties(); 
-         Session session = Session.getDefaultInstance(props, null); 
+         Session session = Session.getDefaultInstance(props, null);
+         
          try {
 			MimeMessage message = new MimeMessage(session, req.getInputStream());
 			
@@ -156,47 +96,7 @@ public class AddEventServlet extends HttpServlet {
     
     
    
-    private EventData getEvent(String subject){
-		    Matcher eventAnnualyMatcher = eventAnnualyPattern.matcher( subject );
-		    Matcher eventOnceMatcher = eventOncePattern.matcher(subject);
-		    EventData data = null;
-            
-		    //match  event
-		    logger.info("match event");
-		    logger.info(subject);
-		    
-		    if (eventAnnualyMatcher.matches() || eventOnceMatcher.matches()){
-		    	logger.info("matching the subject");
-		    	String event = eventAnnualyMatcher.matches() ? eventAnnualyMatcher.group(1) : eventOnceMatcher.group(1);
-		    	logger.info("event :" +event);
-		        int  month = eventAnnualyMatcher.matches() ? Month.getIndex(eventAnnualyMatcher.group(2)) : Month.getIndex(eventOnceMatcher.group(2));
-		        logger.info("month :"+month);
-		        if (month == -1 )
-		        	return data;
-		    	int day = Integer.parseInt(eventAnnualyMatcher.matches() ? eventAnnualyMatcher.group(3) : eventOnceMatcher.group(3));
-		    	logger.info("day : "+day);
-		    	if(eventAnnualyMatcher.matches())
-		    	   data = new EventData("","",event,buildDate(month,day),true,false);
-		    	else
-		    	   data = new EventData("","",event,buildDate(month,day),false,false);
-		    }
-		    return data;
+   
     
-    }
-    
-    private static Date buildDate(int month , int day){
-    	Calendar calendar = Calendar.getInstance();
-    	int currentYear = calendar.get(Calendar.YEAR);
-    	int currentMonth = calendar.get(Calendar.MONTH);
-    	calendar.clear();
-    	if(currentMonth > month || (currentMonth == month && Calendar.DAY_OF_MONTH > day)){
-    		calendar.set(currentYear+1, month, day);
-    		
-    	}
-    	else	
-        	calendar.set(currentYear, month, day);
-    	
-    	logger.info("The date is : "+calendar.toString());
-    	return calendar.getTime();
-    }
+   
 }
